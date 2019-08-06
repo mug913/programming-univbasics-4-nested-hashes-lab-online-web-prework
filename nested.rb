98b2d658370c1dda4faa0f7bed714a3389ad2a73
Brad Alexander <brad.alexander913@gmail.com>
2019-08-06 01:17:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages].
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 return programmer_hash
 end
